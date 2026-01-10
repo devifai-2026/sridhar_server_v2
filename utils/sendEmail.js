d0c1167ac0d2b943f1dcdd7bae4d22d7e8b13c45
sayanpal469 <sayanpal469@gmail.com>
2026-01-10 19:05:57 +0530

    t

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -2,13 +2,10 @@ import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 import { config } from "../config/config.js";
 
-
 export const sendEmail = async ({ to, subject, html }) => {
   try {
     const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 465, // or 587 for TLS
-      secure: true, // true for 465, false for 587
+      service: "gmail",
       auth: {
         user: config.EMAIL_USER, // your Gmail
         pass: config.EMAIL_PASS, // Gmail App Password
